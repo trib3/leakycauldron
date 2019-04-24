45d82184812a2475213c30e72c8e4020a7ca7bda
Tyler <46464238+Tbilgere-td@users.noreply.github.com>
2019-04-23 18:21:16 -0700

    Java.time Scalars
    -Added LocalDate, LocalTime, LocalDateTime, and OffsetDateTime Scalars
    -Added Testing for new Scalars

--- common-server/src/main/kotlin/com/trib3/server/graphql/DateTimeHooks.kt
+++ common-server/src/main/kotlin/com/trib3/server/graphql/DateTimeHooks.kt
@@ -7,6 +7,10 @@ import graphql.schema.CoercingSerializeException
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
 import org.threeten.extra.YearQuarter
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.OffsetDateTime
 import java.time.YearMonth
 import kotlin.reflect.KType
 
@@ -74,6 +78,136 @@ internal val YEAR_QUARTER_SCALAR = GraphQLScalarType.newScalar()
     })
     .build()
 
+internal val LOCAL_DATETIME_SCALAR = GraphQLScalarType.newScalar()
+    .name("LocalDateTime")
+    .description("Year + Month + Day Of Month + Time (Hour:Minute + Optional(Second:Milliseconds)), " +
+        "for example 2019-10-31T12:31:45.129")
+    .coercing(object : Coercing<LocalDateTime, String> {
+        private fun parse(input: String): LocalDateTime {
+            return try {
+                LocalDateTime.parse(input)
+            } catch (e: Exception) {
+                throw CoercingSerializeException("can't parse $input", e)
+            }
+        }
+
+        override fun parseValue(input: Any): LocalDateTime {
+            return parse(input.toString())
+        }
+
+        override fun parseLiteral(input: Any): LocalDateTime {
+            return when (input) {
+                is StringValue -> parse(input.value)
+                else -> throw CoercingSerializeException("can't parse $input")
+            }
+        }
+
+        override fun serialize(dataFetcherResult: Any): String {
+            return when (dataFetcherResult) {
+                is LocalDateTime -> dataFetcherResult.toString()
+                else -> throw CoercingSerializeException("can't serialize ${dataFetcherResult::class}")
+            }
+        }
+    })
+    .build()
+
+internal val LOCAL_DATE_SCALAR = GraphQLScalarType.newScalar()
+    .name("LocalDate")
+    .description("Year + Month + Day Of Month, for example 2019-10-31")
+    .coercing(object : Coercing<LocalDate, String> {
+        private fun parse(input: String): LocalDate {
+            return try {
+                LocalDate.parse(input)
+            } catch (e: Exception) {
+                throw CoercingSerializeException("can't parse $input", e)
+            }
+        }
+
+        override fun parseValue(input: Any): LocalDate {
+            return parse(input.toString())
+        }
+
+        override fun parseLiteral(input: Any): LocalDate {
+            return when (input) {
+                is StringValue -> parse(input.value)
+                else -> throw CoercingSerializeException("can't parse $input")
+            }
+        }
+
+        override fun serialize(dataFetcherResult: Any): String {
+            return when (dataFetcherResult) {
+                is LocalDate -> dataFetcherResult.toString()
+                else -> throw CoercingSerializeException("can't serialize ${dataFetcherResult::class}")
+            }
+        }
+    })
+    .build()
+
+internal val LOCAL_TIME_SCALAR = GraphQLScalarType.newScalar()
+    .name("LocalTime")
+    .description("Hour + Minute + Optional (Second + Millisecond) , for example 12:31:45.129")
+    .coercing(object : Coercing<LocalTime, String> {
+        private fun parse(input: String): LocalTime {
+            return try {
+                LocalTime.parse(input)
+            } catch (e: Exception) {
+                throw CoercingSerializeException("can't parse $input", e)
+            }
+        }
+
+        override fun parseValue(input: Any): LocalTime {
+            return parse(input.toString())
+        }
+
+        override fun parseLiteral(input: Any): LocalTime {
+            return when (input) {
+                is StringValue -> parse(input.value)
+                else -> throw CoercingSerializeException("can't parse $input")
+            }
+        }
+
+        override fun serialize(dataFetcherResult: Any): String {
+            return when (dataFetcherResult) {
+                is LocalTime -> dataFetcherResult.toString()
+                else -> throw CoercingSerializeException("can't serialize ${dataFetcherResult::class}")
+            }
+        }
+    })
+    .build()
+
+internal val OFFSET_DATETIME_SCALAR = GraphQLScalarType.newScalar()
+    .name("OffsetDateTime")
+    .description("Year + Month + Day Of Month + Time (Hour:Minute + Optional(Second.Milliseconds)) " +
+        "+ Offset, for example 2019-10-31T12:31:45.129-07:00")
+    .coercing(object : Coercing<OffsetDateTime, String> {
+        private fun parse(input: String): OffsetDateTime {
+            return try {
+                OffsetDateTime.parse(input)
+            } catch (e: Exception) {
+                throw CoercingSerializeException("can't parse $input", e)
+            }
+        }
+
+        override fun parseValue(input: Any): OffsetDateTime {
+            return parse(input.toString())
+        }
+
+        override fun parseLiteral(input: Any): OffsetDateTime {
+            return when (input) {
+                is StringValue -> parse(input.value)
+                else -> throw CoercingSerializeException("can't parse $input")
+            }
+        }
+
+        override fun serialize(dataFetcherResult: Any): String {
+            return when (dataFetcherResult) {
+                is OffsetDateTime -> dataFetcherResult.toString()
+                else -> throw CoercingSerializeException("can't serialize ${dataFetcherResult::class}")
+            }
+        }
+    })
+    .build()
+
 /**
  * Schema generator hooks implementation that defines scalars for java.time (and threeten-extras) objects
  */
@@ -82,6 +216,10 @@ class DateTimeHooks : SchemaGeneratorHooks {
         return when (type.classifier) {
             YearMonth::class -> YEAR_MONTH_SCALAR
             YearQuarter::class -> YEAR_QUARTER_SCALAR
+            LocalDateTime::class -> LOCAL_DATETIME_SCALAR
+            LocalDate::class -> LOCAL_DATE_SCALAR
+            LocalTime::class -> LOCAL_TIME_SCALAR
+            OffsetDateTime::class -> OFFSET_DATETIME_SCALAR
             else -> null
         }
     }
--- common-server/src/test/kotlin/com/trib3/server/graphql/DateTimeHooksTest.kt
+++ common-server/src/test/kotlin/com/trib3/server/graphql/DateTimeHooksTest.kt
@@ -11,7 +11,12 @@ import graphql.GraphQL
 import graphql.schema.CoercingSerializeException
 import org.testng.annotations.Test
 import org.threeten.extra.YearQuarter
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.OffsetDateTime
 import java.time.YearMonth
+import java.time.ZoneOffset
 
 class DateTimeQuery {
     fun quarter(q: YearQuarter): YearQuarter {
@@ -20,6 +25,22 @@ class DateTimeQuery {
 
     fun month(m: YearMonth): YearMonth {
         return m.plusMonths(1)
+    }
+
+    fun localDateTime(l: LocalDateTime): LocalDateTime {
+        return l.plusDays(1).plusHours(1)
+    }
+
+    fun localDate(l: LocalDate): LocalDate {
+        return l.plusDays(1)
+    }
+
+    fun localTime(l: LocalTime): LocalTime {
+        return l.plusHours(1)
+    }
+
+    fun offsetDateTime(o: OffsetDateTime): OffsetDateTime {
+        return o.plusDays(1)
     }
 }
 
@@ -49,6 +70,11 @@ class DateTimeHooksTest {
             YEAR_QUARTER_SCALAR.coercing.serialize(123)
         }.thrownError {
             isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            YEAR_QUARTER_SCALAR.coercing.serialize(YearQuarter.of(2019, 2))
+        }.returnedValue {
+            isEqualTo("2019-Q2")
         }
     }
 
@@ -81,6 +107,11 @@ class DateTimeHooksTest {
         }.thrownError {
             isInstanceOf(CoercingSerializeException::class)
         }
+        assertThat {
+            YEAR_MONTH_SCALAR.coercing.serialize(YearMonth.of(2019, 10))
+        }.returnedValue {
+            isEqualTo("2019-10")
+        }
     }
 
     @Test
@@ -91,5 +122,154 @@ class DateTimeHooksTest {
                 .variables(mapOf("input" to "2019-01")).build()
         ).getData<Map<String, String>>()
         assertThat(result["month"]).isEqualTo("2019-02")
+    }
+
+    @Test
+    fun testLocalDateTime() {
+        val result = graphQL.execute("""query {localDateTime(l:"2019-10-30T00:01")}""")
+            .getData<Map<String, String>>()
+        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01")
+        assertThat {
+            graphQL.execute("""query {localDateTime(l:123)}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            graphQL.execute("""query {localDateTime(l:"123")}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_DATETIME_SCALAR.coercing.serialize(123)
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_DATETIME_SCALAR.coercing.serialize(LocalDateTime.of(2019, 10, 31, 1, 1))
+        }.returnedValue {
+            isEqualTo("2019-10-31T01:01")
+        }
+    }
+
+    @Test
+    fun testLocalDateTimeVariable() {
+        val result = graphQL.execute(
+            ExecutionInput.newExecutionInput()
+                .query("""query(${'$'}input: LocalDateTime!) {localDateTime(l:${'$'}input)}""")
+                .variables(mapOf("input" to "2019-10-30T00:01")).build()
+        ).getData<Map<String, String>>()
+        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01")
+    }
+
+    @Test
+    fun testLocalDate() {
+        val result = graphQL.execute("""query {localDate(l:"2019-10-30")}""")
+            .getData<Map<String, String>>()
+        assertThat(result["localDate"]).isEqualTo("2019-10-31")
+        assertThat {
+            graphQL.execute("""query {localDate(l:123)}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            graphQL.execute("""query {localDate(l:"123")}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_DATE_SCALAR.coercing.serialize(123)
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_DATE_SCALAR.coercing.serialize(LocalDate.of(2019, 10, 31))
+        }.returnedValue {
+            isEqualTo("2019-10-31")
+        }
+    }
+
+    @Test
+    fun testLocalDateVariable() {
+        val result = graphQL.execute(
+            ExecutionInput.newExecutionInput()
+                .query("""query(${'$'}input: LocalDate!) {localDate(l:${'$'}input)}""")
+                .variables(mapOf("input" to "2019-10-30")).build()
+        ).getData<Map<String, String>>()
+        assertThat(result["localDate"]).isEqualTo("2019-10-31")
+    }
+
+    @Test
+    fun testLocalTime() {
+        val result = graphQL.execute("""query {localTime(l:"00:01")}""")
+            .getData<Map<String, String>>()
+        assertThat(result["localTime"]).isEqualTo("01:01")
+        assertThat {
+            graphQL.execute("""query {localTime(l:123)}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            graphQL.execute("""query {localTime(l:"123")}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_TIME_SCALAR.coercing.serialize(123)
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            LOCAL_TIME_SCALAR.coercing.serialize(LocalTime.of(1, 1))
+        }.returnedValue {
+            isEqualTo("01:01")
+        }
+    }
+
+    @Test
+    fun testLocalTimeVariable() {
+        val result = graphQL.execute(
+            ExecutionInput.newExecutionInput()
+                .query("""query(${'$'}input: LocalTime!) {localTime(l:${'$'}input)}""")
+                .variables(mapOf("input" to "00:01")).build()
+        ).getData<Map<String, String>>()
+        assertThat(result["localTime"]).isEqualTo("01:01")
+    }
+
+    @Test
+    fun testOffsetDateTime() {
+        val result = graphQL.execute("""query {offsetDateTime(o:"2019-10-30T00:01-07:00")}""")
+            .getData<Map<String, String>>()
+        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T00:01-07:00")
+        assertThat {
+            graphQL.execute("""query {offsetDateTime(o:123)}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            graphQL.execute("""query {offsetDateTime(o:"123")}""")
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            OFFSET_DATETIME_SCALAR.coercing.serialize(123)
+        }.thrownError {
+            isInstanceOf(CoercingSerializeException::class)
+        }
+        assertThat {
+            OFFSET_DATETIME_SCALAR.coercing.serialize(
+                OffsetDateTime.of(2019, 10, 31, 1, 1, 31, 129, ZoneOffset.ofHours(-7)))
+        }.returnedValue {
+            isEqualTo("2019-10-31T01:01:31.000000129-07:00")
+        }
+    }
+
+    @Test
+    fun testOffsetDateTimeVariable() {
+        val result = graphQL.execute(
+            ExecutionInput.newExecutionInput()
+                .query("""query(${'$'}input: OffsetDateTime!) {offsetDateTime(o:${'$'}input)}""")
+                .variables(mapOf("input" to "2019-10-30T00:01-07:00")).build()
+        ).getData<Map<String, String>>()
+        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T00:01-07:00")
     }
 }
