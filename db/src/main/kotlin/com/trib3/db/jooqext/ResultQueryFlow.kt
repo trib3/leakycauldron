1830759b5e57a9b67ec442313af89e6fc461f0a8
Joe Barnett <jbarnett@tribedynamics.com>
2020-03-30 11:43:03 -0700

    Add jOOQ ResultQuery Flow adapter
    For a ResultQuery<T>, add a consumeAsFlow() extension
    method that:
     - allows clients to consume a Flow<T>
     - ensures that the underlying query Cursor gets close()d
       on successful or unsuccessful completion
     - monitors the coroutine execution for cancellation,
       and calls query.cancel() to stop any synchronous JDBC
       operations that are blocking execution

---
+++ db/src/main/kotlin/com/trib3/db/jooqext/ResultQueryFlow.kt
@@ -0,0 +1,66 @@
+package com.trib3.db.jooqext
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import org.jooq.Record
+import org.jooq.ResultQuery
+import kotlin.coroutines.CoroutineContext
+
+private val log = KotlinLogging.logger {}
+
+/**
+ * Convert a [ResultQuery] to a [Flow], ensuring that the underlying cursor gets closed on completion,
+ * and that the query is cancelled when the flow consuming coroutine is cancelled.
+ *
+ * Will run [ResultQuery.fetchLazy] in a new coroutine launched with the passed [coroutineContext].
+ * Will run an additional coroutine launched with the passed [coroutineContext] that monitors the
+ * fetch-ing coroutine for cancellation, and calls [ResultQuery.cancel] when that occurs.
+ */
+fun <T : Record> ResultQuery<T>.consumeAsFlow(coroutineContext: CoroutineContext = Dispatchers.IO): Flow<T> {
+    val query = this
+    return flow {
+        coroutineScope {
+            val fetchJob = async(coroutineContext) {
+                query.fetchLazy()
+            }
+            launch(coroutineContext) {
+                // wait until the fetch job is cancelled or complete before doing anything
+                log.trace("fetch monitor awaiting job cancellation or completion")
+                val cancelException = try {
+                    while (fetchJob.isActive) {
+                        delay(100)
+                    }
+                    null
+                } catch (e: CancellationException) {
+                    e
+                }
+                log.trace("fetch monitor awaiting job completion")
+                // wait until the fetch job is actually complete, cancelling the query to speed its completion
+                while (!fetchJob.isCompleted) {
+                    query.cancel()
+                    delay(100)
+                }
+                log.trace("fetch monitor job finished, throwing $cancelException")
+                if (cancelException != null) {
+                    throw cancelException
+                }
+            }
+            val cursor = fetchJob.await()
+            try {
+                for (i in cursor) {
+                    emit(i)
+                }
+            } finally {
+                log.debug("Closing cursor")
+                cursor.close()
+            }
+        }
+    }
+}
