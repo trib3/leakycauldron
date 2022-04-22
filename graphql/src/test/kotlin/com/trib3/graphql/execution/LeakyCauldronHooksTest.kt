bfc97af9c7f3ec683720192e48960b16da6b863e
Joe Barnett <joe.barnett@creatoriq.com>
2022-04-21 17:25:57 -0700

    Use graphql-java-extended-scalars implementations [BREAKING CHANGE]
    For UUID and date/time/datetime types, use available upstream
    implementations instead of maintaining our own.  Note that
    the `OffsetDateTime` GraphQL type is renamed to `DateTime`,
    and the `LocalDate` GraphQL type is renamed to `Date`.  Serialized
    datetimes will also better conform to ISO/RFC formats.
    
    Also update our scalar implementations to throw the appropriate
    exception type, resulting in queries that execute and return
    error results instead of failing to execute.  This is compatible
    with the graphql-java-extended-scalars implementations and more
    in line with how GraphQL failures should be handled anyway.
    
    Bump to version 1.31.x due to these behavioral changes.

--- graphql/src/test/kotlin/com/trib3/graphql/execution/LeakyCauldronHooksTest.kt
+++ graphql/src/test/kotlin/com/trib3/graphql/execution/LeakyCauldronHooksTest.kt
@@ -10,6 +10,7 @@ import com.expediagroup.graphql.generator.TopLevelObject
 import com.expediagroup.graphql.generator.execution.SimpleKotlinDataFetcherFactoryProvider
 import com.expediagroup.graphql.generator.toSchema
 import com.trib3.json.ObjectMapperProvider
+import graphql.ErrorType
 import graphql.ExecutionInput
 import graphql.GraphQL
 import graphql.schema.CoercingSerializeException
@@ -22,7 +23,6 @@ import java.time.LocalTime
 import java.time.OffsetDateTime
 import java.time.Year
 import java.time.YearMonth
-import java.time.ZoneOffset
 import java.util.UUID
 
 class HooksQuery {
@@ -82,16 +82,21 @@ class LeakyCauldronHooksTest {
             toSchema(config, listOf(TopLevelObject(HooksQuery())))
         ).build()
 
+    /**
+     * Assert that executing each of the [invalidQueries] results in a validation error
+     */
+    private fun assertValidationErrors(vararg invalidQueries: String) {
+        for (query in invalidQueries) {
+            val result = graphQL.execute(query)
+            assertThat(result.errors[0].errorType).isEqualTo(ErrorType.ValidationError)
+        }
+    }
+
     @Test
     fun testYear() {
         val result = graphQL.execute("""query {year(y:"2019")}""").getData<Map<String, String>>()
         assertThat(result["year"]).isEqualTo("2020")
-        assertThat {
-            graphQL.execute("""query {year(y:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-        assertThat {
-            graphQL.execute("""query {year(y:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
+        assertValidationErrors("""query {year(y:123)}""", """query {year(y:"123")}""")
 
         assertThat {
             YEAR_SCALAR.coercing.serialize(123)
@@ -116,12 +121,7 @@ class LeakyCauldronHooksTest {
     fun testQuarter() {
         val result = graphQL.execute("""query {quarter(q:"2019-Q1")}""").getData<Map<String, String>>()
         assertThat(result["quarter"]).isEqualTo("2019-Q2")
-        assertThat {
-            graphQL.execute("""query {quarter(q:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-        assertThat {
-            graphQL.execute("""query {quarter(q:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
+        assertValidationErrors("""query {quarter(q:123)}""", """query {quarter(q:"123")}""")
 
         assertThat {
             YEAR_QUARTER_SCALAR.coercing.serialize(123)
@@ -146,13 +146,7 @@ class LeakyCauldronHooksTest {
     fun testMonth() {
         val result = graphQL.execute("""query {month(m:"2019-01")}""").getData<Map<String, String>>()
         assertThat(result["month"]).isEqualTo("2019-02")
-        assertThat {
-            graphQL.execute("""query {month(m:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {month(m:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
+        assertValidationErrors("""query {month(m:123)}""", """query {month(m:"123")}""")
 
         assertThat {
             YEAR_MONTH_SCALAR.coercing.serialize(123)
@@ -177,14 +171,8 @@ class LeakyCauldronHooksTest {
     fun testLocalDateTime() {
         val result = graphQL.execute("""query {localDateTime(l:"2019-10-30T00:01")}""")
             .getData<Map<String, String>>()
-        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01")
-        assertThat {
-            graphQL.execute("""query {localDateTime(l:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {localDateTime(l:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
+        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01:00")
+        assertValidationErrors("""query {localDateTime(l:123)}""", """query {localDateTime(l:"123")}""")
 
         assertThat {
             LOCAL_DATETIME_SCALAR.coercing.serialize(123)
@@ -192,7 +180,7 @@ class LeakyCauldronHooksTest {
 
         assertThat {
             LOCAL_DATETIME_SCALAR.coercing.serialize(LocalDateTime.of(2019, 10, 31, 1, 1))
-        }.isSuccess().isEqualTo("2019-10-31T01:01")
+        }.isSuccess().isEqualTo("2019-10-31T01:01:00")
     }
 
     @Test
@@ -202,7 +190,7 @@ class LeakyCauldronHooksTest {
                 .query("""query(${'$'}input: LocalDateTime!) {localDateTime(l:${'$'}input)}""")
                 .variables(mapOf("input" to "2019-10-30T00:01")).build()
         ).getData<Map<String, String>>()
-        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01")
+        assertThat(result["localDateTime"]).isEqualTo("2019-10-31T01:01:00")
     }
 
     @Test
@@ -210,28 +198,14 @@ class LeakyCauldronHooksTest {
         val result = graphQL.execute("""query {localDate(l:"2019-10-30")}""")
             .getData<Map<String, String>>()
         assertThat(result["localDate"]).isEqualTo("2019-10-31")
-        assertThat {
-            graphQL.execute("""query {localDate(l:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {localDate(l:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            LOCAL_DATE_SCALAR.coercing.serialize(123)
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            LOCAL_DATE_SCALAR.coercing.serialize(LocalDate.of(2019, 10, 31))
-        }.isSuccess().isEqualTo("2019-10-31")
+        assertValidationErrors("""query {localDate(l:123)}""", """query {localDate(l:"123")}""")
     }
 
     @Test
     fun testLocalDateVariable() {
         val result = graphQL.execute(
             ExecutionInput.newExecutionInput()
-                .query("""query(${'$'}input: LocalDate!) {localDate(l:${'$'}input)}""")
+                .query("""query(${'$'}input: Date!) {localDate(l:${'$'}input)}""")
                 .variables(mapOf("input" to "2019-10-30")).build()
         ).getData<Map<String, String>>()
         assertThat(result["localDate"]).isEqualTo("2019-10-31")
@@ -241,22 +215,8 @@ class LeakyCauldronHooksTest {
     fun testLocalTime() {
         val result = graphQL.execute("""query {localTime(l:"00:01")}""")
             .getData<Map<String, String>>()
-        assertThat(result["localTime"]).isEqualTo("01:01")
-        assertThat {
-            graphQL.execute("""query {localTime(l:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {localTime(l:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            LOCAL_TIME_SCALAR.coercing.serialize(123)
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            LOCAL_TIME_SCALAR.coercing.serialize(LocalTime.of(1, 1))
-        }.isSuccess().isEqualTo("01:01")
+        assertThat(result["localTime"]).isEqualTo("01:01:00")
+        assertValidationErrors("""query {localTime(l:123)}""", """query {localTime(l:"123")}""")
     }
 
     @Test
@@ -266,41 +226,25 @@ class LeakyCauldronHooksTest {
                 .query("""query(${'$'}input: LocalTime!) {localTime(l:${'$'}input)}""")
                 .variables(mapOf("input" to "00:01")).build()
         ).getData<Map<String, String>>()
-        assertThat(result["localTime"]).isEqualTo("01:01")
+        assertThat(result["localTime"]).isEqualTo("01:01:00")
     }
 
     @Test
     fun testOffsetDateTime() {
         val result = graphQL.execute("""query {offsetDateTime(o:"2019-10-30T00:01-07:00")}""")
             .getData<Map<String, String>>()
-        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T07:01Z")
-        assertThat {
-            graphQL.execute("""query {offsetDateTime(o:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {offsetDateTime(o:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            OFFSET_DATETIME_SCALAR.coercing.serialize(123)
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            OFFSET_DATETIME_SCALAR.coercing.serialize(
-                OffsetDateTime.of(2019, 10, 31, 1, 1, 31, 129, ZoneOffset.ofHours(-7))
-            )
-        }.isSuccess().isEqualTo("2019-10-31T01:01:31.000000129-07:00")
+        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T07:01:00Z")
+        assertValidationErrors("""query {offsetDateTime(o:123)}""", """query {offsetDateTime(o:"123")}""")
     }
 
     @Test
     fun testOffsetDateTimeVariable() {
         val result = graphQL.execute(
             ExecutionInput.newExecutionInput()
-                .query("""query(${'$'}input: OffsetDateTime!) {offsetDateTime(o:${'$'}input)}""")
+                .query("""query(${'$'}input: DateTime!) {offsetDateTime(o:${'$'}input)}""")
                 .variables(mapOf("input" to "2019-10-30T00:01-07:00")).build()
         ).getData<Map<String, String>>()
-        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T07:01Z")
+        assertThat(result["offsetDateTime"]).isEqualTo("2019-10-31T07:01:00Z")
     }
 
     @Test
@@ -318,17 +262,7 @@ class LeakyCauldronHooksTest {
         val result = graphQL.execute("""query { existinguuid(uuid:"$uuid") }""")
             .getData<Map<String, String>>()
         assertThat(result["existinguuid"]).isEqualTo(uuid.toString())
-        assertThat {
-            graphQL.execute("""query {existinguuid(uuid:123)}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            graphQL.execute("""query {existinguuid(uuid:"123")}""")
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
-
-        assertThat {
-            UUID_SCALAR.coercing.serialize(123)
-        }.isFailure().isInstanceOf(CoercingSerializeException::class)
+        assertValidationErrors("""query {existinguuid(uuid:123)}""", """query {existinguuid(uuid:"123")}""")
     }
 
     @Test
